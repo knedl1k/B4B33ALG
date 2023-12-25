f935cab24757c5cdd5f98147fb4b60955ba6391f
knedl1k <knedl1k@duck.com>
2023-12-25 23:19:23 +0100

    add template files

---
+++ assignments/hw07simulator/main.c
@@ -0,0 +1,13 @@
+//GNU General Public License v3.0
+//@knedl1k
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void){
+ 
+  return 0;
+}
