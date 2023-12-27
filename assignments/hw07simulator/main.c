eb2b56f0a34b51681890fc14bf58d9d6fa2d844a
knedl1k <knedl1k@duck.com>
2023-12-27 15:12:16 +0100

    fixed string parsing

--- assignments/hw07simulator/main.c
+++ assignments/hw07simulator/main.c
@@ -8,65 +8,44 @@
 #include <string.h>
 
 bool foundString(const char *s1,const char *s2);
-static void pPrint(const size_t S, const size_t K, char allowed[S][K]);
 
-static void power(const size_t S, size_t a[S][S], size_t n, size_t res[S][S]);
-static void mul(const size_t S, size_t a[S][S], size_t b[S][S]);
-static void one(const size_t S, size_t a[S][S]);
+static void powerMatrix(const size_t S, size_t mat[S][S], size_t n, size_t result_mat[S][S]);
+static void multiply(const size_t S, size_t a[S][S], size_t b[S][S]);
+static void generateIdentity(const size_t S, size_t a[S][S]);
 
 int main(void){
   size_t S, K, M;
   scanf("%zu %zu %zu",&S, &K, &M);
-  printf("S: %zu, K:%zu, M:%zu\n",S,K,M);
   char allowed[S][K];
-  for(size_t i=0;i<S;++i){
-    for(size_t j=0;j<K;++j){
+  for(size_t i=0;i<S;++i)
+    for(size_t j=0;j<K;++j)
       scanf(" %c ",&allowed[i][j]);
-      printf("%c",allowed[i][j]);
-    }
-    printf("<\n");
-  }
-
-  //pPrint(S, K, allowed);
-
-  //bool v[S];
-  //for(size_t i=0;i<S;++i)
-  //  v[i]=true;
 
   size_t mat[S][S];
 
-  
-  char s1[K-1];
-  char s2[K-1];
-  printf("%zu\n",K-1);
+  char s1[K], s2[K];
   for(size_t i=0;i<S;++i){
     for(size_t k=1;k<K;++k)
       s1[k-1]=allowed[i][k];
-    
+    s1[K-1]='\0';
+
     for(size_t j=0;j<S;++j){
       for(size_t k=0;k<K-1;++k)
         s2[k]=allowed[j][k];
-      printf(">%s< >%s<\n", s1, s2);
+      s2[K-1]='\0';
+      
       mat[i][j]=foundString(s1, s2);
     }
   }
-  
-  for(size_t i=0;i<S;++i){
-    for(size_t j=0;j<S;++j)
-      printf("%zu",mat[i][j]);
-    printf("\n");
-  }
-  
   size_t res[S][S];
-  power(S, mat, M-K, res);
+  powerMatrix(S, mat, M-K, res);
 
   size_t final=0;
   for(size_t i=0;i<S;++i)
     for(size_t j=0;j<S;++j)
       final+=res[i][j];
-    
   
-  printf("final: %zu\n",final);
+  printf("%zu\n",final);
   return 0;
 }
 
@@ -74,56 +53,45 @@ bool foundString(const char *s1,const char *s2){
   return(strcmp(s1, s2)==0)? true:false; 
 }
 
-//It's simple E matrix
-// 1 0 ... 0
-// 0 1 ... 0
-// ....
-// 0 0 ... 1
-static void one(const size_t S, size_t a[S][S]){
-  for(size_t i=0;i<S;++i)
-    for(size_t j=0;j<S;++j)
-      a[i][j]=(i == j);
-}
-
-//Multiply matrix a to matrix b and print result into a
-static void mul(const size_t S, size_t a[S][S], size_t b[S][S]){
-  size_t res[S][S];
-  for(size_t i=0;i<S;++i)
-    for(size_t j=0;j<S;++j)
-      res[i][j]=0;
-  
-  for(size_t i=0;i<S;++i)
-    for(size_t j=0;j<S;++j)
-      for(size_t k=0;k<S;++k)
-        res[i][j] += a[i][k] * b[k][j];
-      
-  for(size_t i=0;i<S;++i)
-    for(size_t j=0;j<S;++j)
-      a[i][j]=res[i][j];
-}
-
-//Caluclate a^n and print result into matrix res
-static void power(const size_t S, size_t a[S][S], size_t n, size_t res[S][S]){
-  one(S,res);
+//caluclate mat^n and store result into result_mat
+static void powerMatrix(const size_t S, size_t mat[S][S], size_t n, size_t result_mat[S][S]){
+  generateIdentity(S,result_mat);
 
   while(n > 0){
-    //printf("meow\n");
     if(n % 2 == 0){
-      mul(S,a,a);
+      multiply(S,mat,mat);
       n /= 2;
     }else{
-      mul(S,res, a);
+      multiply(S,result_mat,mat);
       --n;
     }
   }
 }
 
+//multiply mat1 to mat2 and store result into mat1
+static void multiply(const size_t S, size_t mat1[S][S], size_t mat2[S][S]){
+  size_t tmp[S][S];
+  for(size_t i=0;i<S;++i)
+    for(size_t j=0;j<S;++j)
+      tmp[i][j]=0;
+  
+  for(size_t i=0;i<S;++i)
+    for(size_t j=0;j<S;++j)
+      for(size_t k=0;k<S;++k)
+        tmp[i][j] += mat1[i][k] * mat2[k][j];
+      
+  for(size_t i=0;i<S;++i)
+    for(size_t j=0;j<S;++j)
+      mat1[i][j]=tmp[i][j];
+}
 
-static void pPrint(const size_t S, const size_t K, char allowed[S][K]){
-  for(size_t i=0;i<S;++i){
-    for(size_t j=0;j<K;++j){
-      printf("%c",allowed[i][j]);
-    }
-    printf("\n");
-  }
+//E matrix
+// 1 0 ... 0
+// 0 1 ... 0
+// ....
+// 0 0 ... 1
+static void generateIdentity(const size_t S, size_t mat[S][S]){
+  for(size_t i=0;i<S;++i)
+    for(size_t j=0;j<S;++j)
+      mat[i][j]=(i == j);
 }
